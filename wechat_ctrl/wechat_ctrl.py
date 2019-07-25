8f5606abb6054972016c40f0be8acad4ec90a052
ybsdegit <ybsdeyx@163.com>
2019-07-25 23:16:59 +0800

    wechat 收到消息自动回复

---
+++ wechat_ctrl/wechat_ctrl.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/25 20:45
+# @Author  : Paulson
+# @File    : wechat_ctrl.py
+# @Software: PyCharm
+# @define  : function
+import random
+from ctypes import windll
+import itchat
+
+
+def get_message_girlfriend():
+    content_list = []
+    with open('honeyed.txt', 'r', encoding='utf-8') as f:
+        contents = f.readlines()
+        for content in contents:
+            content_list.append(content)
+    i = len(content_list)
+    message = content_list[random.randint(0, i-1)]
+    return message
+
+# 消息注册机制
+# 只要接受到的任何的文本消息，就自动调用下方的函数
+@itchat.msg_register(['Text'])
+def message(msg):
+    print(msg)
+    data = msg['Text'].strip()
+    ToUserName = msg['ToUserName']
+    FromUserName = msg['FromUserName']
+    UserName = msg['User']['UserName']
+    try:
+        NickName = msg['User']['NickName']
+    except:
+        NickName = 'self'
+    print(NickName + ': ' + data)
+    message_girlfriend = get_message_girlfriend()
+    NickNames = ['choudashouzb', '安心睡觉']
+    
+    
+    if NickName == 'choudashouzb':
+        if '爱' in data or '宝哥' in data:
+            print('发一段话')
+            itchat.send(message_girlfriend, UserName)
+
+    if NickName in NickNames:
+        print('发一段话')
+        print(message_girlfriend, UserName)
+        itchat.send(message_girlfriend, UserName)
+            
+    if FromUserName == ToUserName:
+        itchat.send(message_girlfriend, 'filehelper')
+ 
+    # 取出发给文件助手的消息
+    if ToUserName == 'filehelper':
+        if data == '锁屏':
+            user32 = windll.LoadLibrary('user32.dll')
+            user32.LockWorkStation()
+        
+        
+def main():
+    itchat.auto_login(hotReload=True)
+    itchat.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 数据库/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/25 19:13
+# @Author  : Paulson
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @define  : function
